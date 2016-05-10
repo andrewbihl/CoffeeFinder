67d6abb37ede5b6c010d75ae449a5a42d5a5192c
andrewbihl <andrewbihlva@gmail.com>
2016-05-10 16:28:42 -0400

    Initial Commit

---
+++ CoffeeFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeFinder
+//
+//  Created by Andrew Bihl on 5/10/16.
+//  Copyright © 2016 Andrew Bihl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoffeeFinder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoffeeFinder
+//
+//  Created by Andrew Bihl on 5/10/16.
+//  Copyright © 2016 Andrew Bihl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
